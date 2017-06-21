8dcb24fc03d67e12b531238df5215de45edc9e49
Mahir Shah <mahir.shah1@gmail.com>
2017-06-21 14:39:40 -0700

    Setup Repository with POC for angle Syntax
    - add scripts to acquire and format data from MDN
    - add basic structure for constants, grammars, formatter, etc
    - add base public functions isShorthandProperty and expandShorthandProperty to be implemented

---
+++ src/data/acquire.js
@@ -0,0 +1,17 @@
+/**
+ * Script used to acquire CSS data from MDN repo. Writes json files out to DATA_PATH.
+ */
+const fetch = require('node-fetch');
+const fs = require('fs-extra');
+const PATHS = require('../constants/paths');
+
+const BASE_URL = 'https://raw.githubusercontent.com/mdn/data/master/css/';
+const FILES_TO_ACQUIRE = ['at-rules', 'properties', 'selectors', 'syntaxes', 'types', 'units'];
+
+Promise
+  .all(FILES_TO_ACQUIRE.map(fileName => fetch(`${BASE_URL}${fileName}.json`)))
+  .then(files => Promise.all(files.map(file => file.json())))
+  .then(resolvedFiles => resolvedFiles
+    .map((file, idx) => fs.writeJson(`${PATHS.DATA_PATH}${FILES_TO_ACQUIRE[idx]}.json`, file, { spaces: 2 })))
+  .then(() => console.log(`Successfully wrote files in ${PATHS.DATA_PATH}`))
+  .catch(err => console.log('error: ', err));
