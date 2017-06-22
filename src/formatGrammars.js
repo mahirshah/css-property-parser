e508d8662f5f3589843301fbda972a75ebb4fb70
Mahir Shah <mashah@linkedin.com>
2017-06-22 16:57:01 -0700

    Add JsonGrammarFormatter and basic format json grammars script
    - add a json grammar formatter that formats a formal syntax into a json grammar
    - add a base implementation for format json grammars script
    - refactor constants and json grammars

--- src/formatGrammars.js
+++ src/formatGrammars.js
@@ -1,8 +1,11 @@
+/**
+ * Format each JSON grammar into an Ohm grammar
+ */
 const fs = require('fs-extra');
 const OhmGrammarFormatter = require('./formatters/OhmGrammarFormatter');
 const PATHS = require('./constants/paths');
 
-const json = fs.readJsonSync('./src/grammars/angle.json');
+const json = fs.readJsonSync('./src/jsonGrammars/angle.json');
 
 fs.outputFile(`${PATHS.OHM_GRAMMAR_PATH}angle.ohm`, OhmGrammarFormatter.formatOhmGrammarFromJson(json, 'Angle'))
   .then(() => console.log(`Successfully wrote grammar to ${PATHS.OHM_GRAMMAR_PATH}`));
