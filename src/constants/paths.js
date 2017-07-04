5f358e6709bb907d684a306c9a4ef612451ac117
Mahir Shah <mahir.shah1@gmail.com>
2017-07-03 21:48:10 -0700

    Correct formatFormalSyntaxes to Generate Json Grammar Files
    - edit formatFormalSyntaxes script to output correct json grammars
    - edit gitignore to ignore generated grammars
    - fix paths to use __dirname

--- src/constants/paths.js
+++ src/constants/paths.js
@@ -1,8 +1,8 @@
 // TODO: use __dirname for all paths
 module.exports = {
-  JSON_GRAMMAR_PATH: './src/jsonGrammars/',
+  JSON_GRAMMAR_PATH: `${__dirname}/../jsonGrammars/`,
   OHM_GRAMMAR_PATH: './ohm-jsonGrammars/',
-  DATA_PATH: './data',
+  DATA_PATH: `${__dirname}/../../data/`,
   FORMATTED_DATA_PATH: './formatted-data/',
   FORMAL_SYNTAX_GRAMMAR_PATH: `${__dirname}/../grammars/`,
 };
--- src/formatFormalSyntaxes.js
+++ src/formatFormalSyntaxes.js
@@ -3,11 +3,33 @@
  */
 const fs = require('fs-extra');
 const ohm = require('ohm-js');
+const PATHS = require('./constants/paths');
 const JsonGrammarFormatter = require('./formatters/JsonGrammarFormatter');
 
+// array of syntax/property names that require manual generation.
+// TODO: export this property and check that manual syntax jsons exist in pre commit
+const manualSyntaxes = ['image()', 'offset'];
 
-const grammarContents = fs.readFileSync('./src/grammars/formalSyntax.ohm');
-const formalSyntaxGrammar = ohm.grammar(grammarContents);
-const s = new JsonGrammarFormatter(formalSyntaxGrammar)
-  .formatFormalSyntax('background', '<bg-image> || <position> [ / <bg-size> ]? || <repeat-style> || <attachment> || <box>{1,2}');
-console.log(s);
+fs.readFile(`${PATHS.FORMAL_SYNTAX_GRAMMAR_PATH}formalSyntax.ohm`)
+  .then(grammarContents => ohm.grammar(grammarContents))
+  .then(formalSyntaxGrammar => new JsonGrammarFormatter(formalSyntaxGrammar))
+  .then(jsonGrammarFormatter => Promise.all([
+    fs.readJson(`${PATHS.DATA_PATH}syntaxes.json`),
+    fs.readJson(`${PATHS.DATA_PATH}properties.json`),
+  ])
+    // combine properties and syntaxes into one object mapping property names to syntaxes
+    .then(([syntaxes, properties]) => Object.entries(properties).reduce((syntaxMap, [propertyName, { syntax }]) => (
+      Object.assign({ [propertyName]: syntax }, syntaxMap)
+    ), syntaxes))
+    .then(Object.entries.bind(Object))
+    .then(syntaxEntries => syntaxEntries
+      // filter out any entries that we need to do manually
+      .filter(([grammarName]) => !manualSyntaxes.includes(grammarName))
+      // format each formal syntax into a json grammar
+      .map(([grammarName, formalSyntax]) => (
+        [grammarName, jsonGrammarFormatter.formatFormalSyntax(grammarName, formalSyntax)]))
+      // write each json grammar to a file
+      .forEach(([grammarName, jsonGrammar]) => (
+        fs.writeJson(`${PATHS.JSON_GRAMMAR_PATH}${grammarName}.json`, jsonGrammar, { spaces: 2 })
+      ))))
+  .catch(e => console.log(e));
