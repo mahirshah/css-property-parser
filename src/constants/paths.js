0282bc6e9b0a1478f0a03dfd3d723c984515f916
Mahir Shah <mahir.shah1@gmail.com>
2017-07-04 17:53:44 -0700

    Use mdn-data NPM package Instead of Scraping Data

--- src/constants/paths.js
+++ src/constants/paths.js
@@ -2,7 +2,6 @@
 module.exports = {
   JSON_GRAMMAR_PATH: `${__dirname}/../jsonGrammars/`,
   OHM_GRAMMAR_PATH: './ohm-jsonGrammars/',
-  DATA_PATH: `${__dirname}/../../data/`,
   FORMATTED_DATA_PATH: './formatted-data/',
   FORMAL_SYNTAX_GRAMMAR_PATH: `${__dirname}/../grammars/`,
 };
