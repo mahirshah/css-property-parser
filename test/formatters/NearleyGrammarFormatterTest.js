c6f4384ea5f5ae21ed053279d731453c1e46efeb
Mahir Shah <mahir.shah1@gmail.com>
2017-08-23 13:19:59 -0700

    Cleanup JsonGrammarFormatter and NearleyFormatter test

--- test/formatters/NearleyGrammarFormatterTest.js
+++ test/formatters/NearleyGrammarFormatterTest.js
@@ -7,9 +7,8 @@ const NearleyGrammarFormatter = require('../../src/formatters/grammarFormatters/
 
 /**
  * Tests for {@link NearleyGrammarFormatter}
- * TODO: come up with a better way to test the formatter
  */
-describe.skip('NearleyGrammarFormatter#format', function () {
+describe('NearleyGrammarFormatter#format', function () {
   let sandbox;
 
   beforeEach(function () {
@@ -26,7 +25,7 @@ describe.skip('NearleyGrammarFormatter#format', function () {
         [GRAMMAR_CONSTANTS.BASE_GRAMMAR_RULE_NAME, '( "test" )'],
       ], 'test');
 
-      assert.equal(result, '@builtin "whitespace.ne"\n\nBase -> ( "test" ) {% function (d,l) { return { name: \'Base\', values: d.filter(Boolean), l }; }  %}');
+      assert.equal(result, '@builtin "whitespace.ne"\n\nBase -> ( "test" )');
     });
   });
 
@@ -53,58 +52,16 @@ describe.skip('NearleyGrammarFormatter#format', function () {
         ['<ra>'],
       ], 'r');
 
-      assert.equal(result, '@builtin "whitespace.ne"\n\nBase -> Ra {% function (d,l) { return { name: \'Base\', values: d.filter(Boolean), l }; }  %}\nRa -> Rb {% id  %}\nRb -> Rc {% id  %}\nRc -> "d" {% id  %}');
+      assert.equal(result, '@builtin "whitespace.ne"\n' +
+        '\n' +
+        'Base -> Ra\n' +
+        'Ra -> Rb\n' +
+        'Rb -> Rc\n' +
+        'Rc -> "d"');
     });
 
     it('should filter out duplicate recursively resolved keys', function () {
 
-    });
-  });
-
-  describe('real use cases', function () {
-    it('should handle hexColor', function () {
-      const result = NearleyGrammarFormatter.format([
-        [GRAMMAR_CONSTANTS.BASE_GRAMMAR_RULE_NAME, '( "#" ( eight | six | four | three ) )'],
-        ['three', 'hexDigit hexDigit hexDigit'],
-        ['four', 'hexDigit hexDigit hexDigit hexDigit'],
-        ['six', 'hexDigit hexDigit hexDigit hexDigit hexDigit hexDigit'],
-        ['eight', 'hexDigit hexDigit hexDigit hexDigit hexDigit hexDigit hexDigit hexDigit'],
-        ['hexDigit', '[0-9A-Fa-f]'],
-      ], 'hexColor');
-
-      assert.equal(result, '');
-    });
-
-    it('should handle number', function () {
-      const result = NearleyGrammarFormatter.format([
-        ['__base__', 'float | exponent'],
-        ['float', '"float"'],
-        ['exponent', '"exponent"'],
-      ], 'number');
-
-      assert.equal(result, 'number {\n  exp = number_float | number_exponent\n  number_float = "float"\n  number_exponent = "exponent"\n}');
-    });
-
-    it('should handle angle', function () {
-      sandbox.stub(fs, 'readJsonSync')
-        .withArgs(`${PATHS.GENERATED_JSON_GRAMMAR_PATH}angle-unit.json`)
-        .returns([[
-          '__base__', '"deg" | "grad" | "rad" | "turn"',
-        ]])
-        .withArgs(`${PATHS.GENERATED_JSON_GRAMMAR_PATH}number.json`)
-        .returns([
-          ['__base__', 'float | scientific'],
-          ['float', '(("+"| "-")? digit* "."? digit+)'],
-          ['scientific', '(("+"| "-")? digit* "."? digit+ "e" "-"? digit+)'],
-        ]);
-
-      const result = NearleyGrammarFormatter.format([
-        ['__base__', '<number> <angle-unit>'],
-        ['<number>'],
-        ['<angle-unit>'],
-      ], 'angle');
-
-      assert.equal(result, 'angle {\n  exp = number angleUnit\n  number = number_float | number_scientific\n  number_float = (("+"| "-")? digit* "."? digit+)\n  number_scientific = (("+"| "-")? digit* "."? digit+ "e" "-"? digit+)\n  angleUnit = "deg" | "grad" | "rad" | "turn"\n}');
     });
   });
 });
