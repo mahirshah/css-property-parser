f80854eec301af6ace29fa22ef3bccf027d982b2
Mahir Shah <mahir.shah1@gmail.com>
2017-09-25 17:20:23 -0700

    Format code comments

--- css-property-parser.d.ts
+++ css-property-parser.d.ts
@@ -6,16 +6,18 @@ declare namespace CssPropertyParser {
   }
 
   /**
-   * Given a property and value attempts to expand the value into its longhand equivalents. Returns an object
-   * mapping the property longhand names to the longhand values. If the property cannot be expanded (i.e. the property
-   * is not a shorthand property) simply returns an object mapping the original property to the original value.
+   * Given a property and value attempts to expand the value into its longhand
+   * equivalents. Returns an object mapping the property longhand names to the
+   * longhand values. If the property cannot be expanded (i.e. the property is
+   * not a shorthand property) simply returns an object mapping the original
+   * property to the original value.
    *
    * @param {string} propertyName - the property name for the given value
    * @param {string} propertyValue - the value of the property
-   * @param {boolean} [recursivelyResolve=true] - recursively resolve additional longhand properties if the shorthands
-   *                                              expand to additional shorthands. For example, the border property
-   *                                              expands to border-width, which expands further to border-left-width,
-   *                                              border-right-width, etc.
+   * @param {boolean} [recursivelyResolve=true] - recursively resolve additional
+   *   longhand properties if the shorthands expand to additional shorthands. For
+   *   example, the border property expands to border-width, which expands
+   *   further to border-left-width, border-right-width, etc.
    */
   function expandShorthandProperty(
     propertyName: string,
