32fc7aec1317a950164a61df233368f279b36344
Mahir Shah <mashah@linkedin.com>
2017-10-12 14:07:05 -0700

    (bug)[Generic Lexer]: Make Generic Lexer Case Insensitive
    - add token transformation to make generic lexer case insensitive
    
    Fixes #7

--- src/constants/genericLexer.js
+++ src/constants/genericLexer.js
@@ -1,12 +1,24 @@
 const moo = require('moo');
 
 module.exports = moo.compile({
-  color: /(?:#(?:[0-9a-fA-F]{2}){2,4}|#[0-9a-fA-F]{3}|(?:rgba?|hsla?)\(\s*(?:\d+%?(?:deg|rad|grad|turn)?(?:,|\s)+){2,3}[\s/]*[\d.]+%?\s*\))/,
+  color: {
+    match: /(?:#(?:[0-9a-fA-F]{2}){2,4}|#[0-9a-fA-F]{3}|(?:rgba?|hsla?)\(\s*(?:\d+%?(?:deg|rad|grad|turn)?(?:,|\s)+){2,3}[\s/]*[\d.]+%?\s*\))/,
+    value: match => match.toLowerCase(),
+  },
   number: /(?:[+-])?(?:(?:\d+\.\d*)|(?:\d*\.\d+))/,
   integer: /(?:[+-])?[0-9]+/,
   string: /(?:"[^"]")|(?:'[^']')/,
   functionStart: /[a-zA-Z]+\(/,
-  customIdent: /[^0-9\s](?:[a-zA-Z0-9_-]|(?:\\.))*/,
-  ident: /[a-zA-Z-]+/,
-  char: /./,
+  customIdent: {
+    match: /[^0-9\s](?:[a-zA-Z0-9_-]|(?:\\.))*/,
+    value: match => match.toLowerCase(),
+  },
+  ident: {
+    match: /[a-zA-Z-]+/,
+    value: match => match.toLowerCase(),
+  },
+  char: {
+    match: /./,
+    value: match => match.toLowerCase(),
+  },
 });
